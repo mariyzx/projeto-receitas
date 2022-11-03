08313cf465f837a8dc76dd0af1bf8aefe00ba703
Mariana Werneck <marinhomariana8@gmail.com>
2022-11-03 12:18:17 -0300

    Redireciona para o perfil ao clicar no ícone

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,15 @@ import React from "react";
 import { CgProfile } from 'react-icons/cg';
 import { BsSearch } from 'react-icons/bs';
 import { Headers } from '../styles/Header';
+import { useHistory } from "react-router-dom";
 
 function Header() {
+  const history = useHistory();
   return (
     <Headers>
       <h1>{window.location.pathname === '/foods' ? 'Foods' : 'Drinks'}</h1>
       <div>
-        <button><CgProfile /></button>
+        <button onClick={() => history.push('/profile')}><CgProfile /></button>
         <button><BsSearch /></button>
       </div>
     </Headers>
