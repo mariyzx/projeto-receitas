7c771f2c8c32cef6a5db9967d62d1aeb276380a3
Mariana Werneck <marinhomariana8@gmail.com>
2022-11-21 17:56:16 -0300

    Implementando botão de favoritar receita

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { AiOutlineShareAlt } from 'react-icons/ai'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import Carousel from "../components/Carousel";
 import Context from "../context/Context";
 import { ShareAndFavButton, StartButton } from "../styles/Buttons";
@@ -11,14 +11,33 @@ import { StyledLink } from "../styles/Recipes";
 function RecipeDetails() {
   const { idMeals, idDrinks } = useParams();
   const [copied, setCopied] = useState(false);
+  const [iconFav, setIconFav] = useState(false)
   const history = useHistory();
-  const { recipe, drinks, foods, waitMeal, waitDrink, measures, ingredients, inProgress } = useContext(Context);
+  const { recipe, 
+    drinks, 
+    foods,
+    waitMeal, 
+    waitDrink, 
+    measures, 
+    ingredients,
+    inProgress,
+    favorites,
+    setIsFavorites } = useContext(Context);
   useEffect(() => {
     setCopied(false);
     const inProgressList = JSON.parse(localStorage.getItem('inProgressRecipes'))
+    const favoritesList = JSON.parse(localStorage.getItem('favoriteRecipes'));
     if (idMeals) { waitMeal(idMeals, inProgressList) }
     if (idDrinks) { waitDrink(idDrinks, inProgressList) }
+    if (idMeals && favoritesList) { verifyFav(); }
   }, [])
+
+  const verifyFav = () => {
+    let icon = false;
+    const favoritesList = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    favoritesList.forEach((food) => food.id === idMeals ? icon = true : icon = false)
+    setIconFav(icon);
+  }
 
   const cond = () => {
    if (history.location.pathname.includes('foods')) {
@@ -33,7 +52,6 @@ function RecipeDetails() {
     navigator.clipboard.writeText(url);
     setCopied(true);
   }
-
 
   const saveThisProgress = () => {
     const inProgressList = JSON.parse(localStorage.getItem('inProgressRecipes'));
@@ -65,6 +83,111 @@ function RecipeDetails() {
     }
   }
 
+  const createNewArray = () => {
+    if (idMeals) {
+      const arrayNew = [{
+        id: idMeals,
+        type: 'food',
+        nationality: recipe.strArea,
+        category: recipe.strCategory,
+        alcoholicOrNot: recipe.strAlcoholic ? recipe.strAlcoholic : '',
+        name: recipe.strMeal ? recipe.strMeal : recipe.strDrink,
+        image: recipe.strMealThumb ? recipe.strMealThumb : recipe.strDrinkThumb,
+        doneDate: '',
+        tags: [recipe.strTags],
+      }];
+      localStorage.setItem('doneRecipes', JSON.stringify(arrayNew));
+      delete arrayNew[0].doneDate;
+      delete arrayNew[0].tags;
+      localStorage.setItem('favoriteRecipes', JSON.stringify(arrayNew));
+    }
+    if (idDrinks) {
+      const arrayNew = [{
+        id: idDrinks,
+        type: 'drink',
+        nationality: '',
+        category: recipe.strCategory,
+        alcoholicOrNot: recipe.strAlcoholic ? recipe.strAlcoholic : '',
+        name: recipe.strMeal ? recipe.strMeal : recipe.strDrink,
+        image: recipe.strMealThumb ? recipe.strMealThumb : recipe.strDrinkThumb,
+        doneDate: '',
+        tags: [recipe.strTags],
+      }];
+      localStorage.setItem('doneRecipes', JSON.stringify(arrayNew));
+      delete arrayNew[0].doneDate;
+      delete arrayNew[0].tags;
+      localStorage.setItem('favoriteRecipes', JSON.stringify(arrayNew));
+    }
+  };
+
+  const refreshTheArray = (doneRecipesList) => {
+    if (idMeals) {
+      const arrayRefresh = [
+        ...doneRecipesList,
+        { id: idMeals,
+          type: 'food',
+          nationality: recipe.strArea,
+          category: recipe.strCategory,
+          alcoholicOrNot: recipe.strAlcoholic ? recipe.strAlcoholic : '',
+          name: recipe.strMeal ? recipe.strMeal : recipe.strDrink,
+          image: recipe.strMealThumb ? recipe.strMealThumb : recipe.strDrinkThumb,
+          doneDate: '',
+          tags: [recipe.strTags],
+        }];
+      localStorage.setItem('doneRecipes', JSON.stringify(arrayRefresh));
+      delete arrayRefresh[0].doneDate;
+      delete arrayRefresh[0].tags;
+      localStorage.setItem('favoriteRecipes', JSON.stringify(arrayRefresh));
+    }
+    if (idDrinks) {
+      const arrayRefresh = [
+        ...doneRecipesList,
+        { id: idDrinks,
+          type: 'drink',
+          nationality: '',
+          category: recipe.strCategory,
+          alcoholicOrNot: recipe.strAlcoholic ? recipe.strAlcoholic : '',
+          name: recipe.strMeal ? recipe.strMeal : recipe.strDrink,
+          image: recipe.strMealThumb ? recipe.strMealThumb : recipe.strDrinkThumb,
+          doneDate: '',
+          tags: [recipe.strTags],
+        }];
+      localStorage.setItem('doneRecipes', JSON.stringify(arrayRefresh));
+      delete arrayRefresh[0].doneDate;
+      delete arrayRefresh[0].tags;
+      localStorage.setItem('favoriteRecipes', JSON.stringify(arrayRefresh));
+    }
+  };
+
+  const removeFavorite = (favoritesList) => {
+    if (idMeals) {
+      const arrayAfterRemoved = favoritesList.filter(({ id }) => id !== idMeals);
+      localStorage.setItem('favoriteRecipes', JSON.stringify(arrayAfterRemoved));
+      verifyFav();
+    }
+    if (idDrinks) {
+      const arrayAfterRemoved = favoritesList.filter(({ id }) => id !== idDrinks);
+      localStorage.setItem('favoriteRecipes', JSON.stringify(arrayAfterRemoved));
+      verifyFav();
+    }
+  };
+
+  const addToFav = () => {
+    if (favorites === false) {
+      setIsFavorites(true);
+      const favoritesList = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      if (favoritesList === null) { createNewArray(); return; }
+      if (favoritesList !== null) { refreshTheArray(favoritesList); }
+      verifyFav();
+    }
+    if (favorites === true) {
+      setIsFavorites(false);
+      const favoritesList = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      removeFavorite(favoritesList);
+      verifyFav();
+    }
+  }
+
   return (
     <div>
       { (drinks.drinks && foods.meals) && recipe.map((item,i) => (
@@ -91,8 +214,12 @@ function RecipeDetails() {
           </Ingredients>
           { copied && 'Link copied!'}
           <ShareAndFav>
-            <ShareAndFavButton onClick={ () => copyRecipe() }><AiOutlineShareAlt /></ShareAndFavButton>
-            <ShareAndFavButton><MdFavoriteBorder /></ShareAndFavButton>
+            <ShareAndFavButton onClick={ () => copyRecipe() }>
+              <AiOutlineShareAlt />
+            </ShareAndFavButton>
+            <ShareAndFavButton onClick={ () => addToFav()}>
+            {iconFav ? <MdFavorite /> : <MdFavoriteBorder />}
+            </ShareAndFavButton>
           </ShareAndFav>
           <StepByStep>
             <h4> Step-By-Step </h4>
